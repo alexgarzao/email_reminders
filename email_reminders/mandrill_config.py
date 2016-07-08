f4836942a8d1c6a2300ecbd672b7a6f9ac687d02
Alex S. Garzao <alexgarzao@gmail.com>
2016-07-08 17:20:44 -0300

    Refactor: Config classes now they read your config and print them.

--- email_reminders/mandrill_config.py
+++ email_reminders/mandrill_config.py
@@ -3,20 +3,36 @@
 # Author: Alex S. Garzão <alexgarzao@gmail.com>
 # mandrill_config.py
 
+import logging
+
 
 class MandrillConfig:
     '''Class responsible in keep the mandrill config.
     '''
 
-    def __init__(
-            self, api_key,
-            track_clicks = True, track_opens = True, tracking_domain = True,
-            google_analytics_campaign = None, google_analytics_domains = None, headers = None, metadata = None):
-        self.api_key = api_key
+    def __init__(self, config, track_clicks = True, track_opens = True, tracking_domain = True):
+
+        self.logger = logging.getLogger(__name__)
+
+        self.api_key = config.get('mandrill', 'api_key')
+        self.google_analytics_campaign = config.get('mandrill', 'google_analytics_campaign')
+        self.google_analytics_domains =  [config.get('mandrill', 'google_analytics_domains')]
+        self.headers = {'Reply-To': config.get('mandrill', 'reply_to')}
+        self.metadata = {'website': config.get('mandrill', 'website')}
+
         self.track_clicks = track_clicks
         self.track_opens = track_opens
         self.tracking_domain = tracking_domain
-        self.google_analytics_campaign = google_analytics_campaign
-        self.google_analytics_domains = google_analytics_domains
-        self.headers = headers
-        self.metadata = metadata
+
+        return
+
+
+    def print_config(self):
+        self.logger.info('\nMANDRILL config')
+        self.logger.info('\tapi_key: %s' % self.api_key)
+        self.logger.info('\tgoogle_analytics_campaign: %s' % self.google_analytics_campaign)
+        self.logger.info('\tgoogle_analytics_domains: %s' % self.google_analytics_domains)
+        self.logger.info('\reply_to: %s' % self.headers)
+        self.logger.info('\twebsite: %s' % self.metadata)
+
+        return
--- email_reminders/reminder_config.py
+++ email_reminders/reminder_config.py
@@ -3,19 +3,41 @@
 # Author: Alex S. Garzão <alexgarzao@gmail.com>
 # reminder_config.py
 
+import logging
+
 
 class ReminderConfig:
     '''Class responsible in keep the reminder config.
     '''
 
-    def __init__(self, action_name, reminder_template_name, summary_template_name, tables, fields, filter, attachment_url, send_resume_to, update, update_field):
-        self.action_name = action_name
-        self.reminder_template_name = reminder_template_name
-        self.summary_template_name = summary_template_name
-        self.tables = tables
-        self.fields = fields
-        self.filter = filter
-        self.attachment_url = attachment_url
-        self.send_resume_to = send_resume_to
-        self.update = update
-        self.update_field = update_field
+    def __init__(self, config):
+        self.logger = logging.getLogger(__name__)
+
+        self.action_name = config.get('reminder', 'action_name')
+        self.reminder_template_name = config.get('reminder', 'reminder_template_name')
+        self.summary_template_name = config.get('reminder', 'summary_template_name')
+        self.tables = config.get('reminder', 'tables')
+        self.fields = config.get('reminder', 'fields')
+        self.filter = config.get('reminder', 'filter')
+        self.attachment_url = config.get('reminder', 'attachment_url')
+        self.send_resume_to = config.get('reminder', 'send_resume_to')
+        self.update = config.get('reminder', 'update')
+        self.update_field = config.get('reminder', 'update_field')
+
+        return
+
+
+    def print_config(self):
+        self.logger.info('\nREMINDER config')
+        self.logger.info('\taction_name: %s' % self.action_name)
+        self.logger.info('\treminder_template_name: %s' % self.reminder_template_name)
+        self.logger.info('\tsummary_template_name: %s' % self.summary_template_name)
+        self.logger.info('\ttables: %s' % self.tables)
+        self.logger.info('\tfields: %s' % self.fields)
+        self.logger.info('\tfilter: %s' % self.filter)
+        self.logger.info('\tattachment_url: %s' % self.attachment_url)
+        self.logger.info('\tsend_resume_to: %s' % self.send_resume_to)
+        self.logger.info('\tupdate: %s' % self.update)
+        self.logger.info('\tupdate_field: %s' % self.update_field)
+
+        return
